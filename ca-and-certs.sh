b7a9dbd91f7f4c4aeca5594c14737c755ab159e3
Jonathan Perry <jonathanperry@sequencemedia.net>
2023-01-11 19:43:31 +0000

    Bash

--- ca-and-certs.sh
+++ ca-and-certs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -z "$1" ]
+if [ -z "$1" ];
 then
   SUBJ="/C=UK/ST=England/L=Greenwich/O=Sequence Media Limited/CN=Sequence Media/emailAddress=sequencemedia@sequencemedia.net"
 else
